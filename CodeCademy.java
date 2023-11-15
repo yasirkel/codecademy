7ab51ce302f140e8d91c2e6555b9201d43062321
bokonendi076 <63402965+bokonendi076@users.noreply.github.com>
2023-11-15 15:38:13 +0100

    Yasir commit

--- CodeCademy.java
+++ CodeCademy.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class CodeCademy {
 
     public static void main(String[] args) {
-        System.out.println("Hij werkt!");
+        System.out.println("Hij werkt!: Yasir");
         System.out.println("Hij werkt");
     }
 }
