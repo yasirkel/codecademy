84d12871ca142311c0096a6cb1f68147d284d4ae
YassineVos <yessinboukrach@hotmail.com>
2023-12-12 13:48:27 +0100

    Test

--- src/CodeCademy.java
+++ src/CodeCademy.java
@@ -9,22 +9,19 @@ import db.Db;
 public class CodeCademy {
 
     public static void main(String[] args) {
-        System.out.println("Hij werkt!: Yasir");
-        System.out.println("Hij werkt: Amin");
-        System.out.println("hij werk: Khalil");
 
         // db klasse aangemaakt en connectie gestart
         Db db = new Db();
         db.connect();
 
         // sla het resultaat van de query op in de resultset.
-        ResultSet resultSet = db.query("SELECT * FROM Users WHERE firstname = 'Yasir'");
+        ResultSet resultSet = db.query("SELECT * FROM Cursist WHERE Name = 'Yassine'");
 
         try {
             // loop door de results
             while (resultSet.next()) {
                 // haal van de resultaten de columnaam op met de value daarin
-                String firstname = resultSet.getString("firstname");
+                String firstname = resultSet.getString("Name");
                 System.out.println(firstname);
             }
         } catch (SQLException e) {
