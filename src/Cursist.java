1e4af50995a761b0817c505a1c467e0742553e14
Amin-Ahmidout <AhmidoutA@hotmail.com>
2023-12-12 13:01:54 +0100

    cursist class amin

---
+++ src/Cursist.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class Cursist {
+    private String emailAddress;
+    private String name;
+    private LocalDate birthDate;
+    private boolean sex;
+    private String address;
+    private String residence;
+    private String country;
+    private int contentItemID;
+
+}
