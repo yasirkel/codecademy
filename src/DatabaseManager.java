0c1f0a706fc7731a8f9794a049bfe84fb0a7f819
Amin-Ahmidout <AhmidoutA@hotmail.com>
2023-12-14 13:15:24 +0100

    niet gebruikte imports verwijderd (amin)

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class DatabaseManager {
     private Connection connection;
